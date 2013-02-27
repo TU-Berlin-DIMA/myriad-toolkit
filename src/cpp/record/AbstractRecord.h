72613c9b70c5cff350aa1a592e34f5223f8fde2b
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2013-02-27 21:14:29 +0100

    Added 'Bool' type alias for 'bool' in types.h, added explicit using statements for 'Date' and 'Bool' types in AbstractRecord.h.

--- src/cpp/record/AbstractRecord.h
+++ src/cpp/record/AbstractRecord.h
@@ -27,16 +27,17 @@
 using namespace std;
 using namespace Poco;
 
+using Myriad::Bool;
+using Myriad::Char;
+using Myriad::Date;
+using Myriad::Decimal;
+using Myriad::Enum;
 using Myriad::I16;
 using Myriad::I32;
 using Myriad::I64;
 using Myriad::I16u;
 using Myriad::I32u;
 using Myriad::I64u;
-using Myriad::Enum;
-using Myriad::Char;
-using Myriad::Decimal;
-using Myriad::Date;
 using Myriad::String;
 
 // forward declarations of auxiliary complex types
